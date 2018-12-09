e364ac7414c8256810885dc84786f6a5e65d23df
Ubuntu <australian.in.tus@icloud.com>
2018-12-09 09:38:35 +0000

    Add a node

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 # pimouse_setup
 test
-=======
-# pimouse_ros
->>>>>>> 5950acaf75196e1b3fd591d4dc0ddd567d02be13
--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,17 +1,14 @@
 #!/bin/bash -xve
 
 #required packages
-pip install catkin_pkg
-pip install empy
-pip install pyyaml
-pip install rospkg
 
 #ros install
 cd ..
-git clone https://github.com/buffalokusojima/ros_setup_scripts_Ubuntu14.04_server.git
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
 cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
+
 
 #catkin setup
 mkdir -p ~/catkin_ws/src
