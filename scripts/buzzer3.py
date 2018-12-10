c748efce02c21891c71a6ac4be2d86ae579d0f8e
Ubuntu <australian.in.tus@icloud.com>
2018-12-10 14:02:45 +0000

    final

--- scripts/buzzer3.py
+++ scripts/buzzer3.py
@@ -6,7 +6,7 @@ def write_freq(hz=0):
     bfile = "/dev/rtbuzzer0"
     try:
         with open(bfile,"w") as f:
-            f.write(str(hs) + "\n")
+            f.write(str(hz) + "\n")
     except IOError:
         rospy.logerr("can't write to " + bfile)
 
