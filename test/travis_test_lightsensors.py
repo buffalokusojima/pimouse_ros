35810b7d2ee199c87d36b7740c132ac9681edcf1
Ubuntu <australian.in.tus@icloud.com>
2018-12-24 12:24:39 +0000

    print

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -26,7 +26,7 @@ class LightSensorTest(unittest.TestCase):
 
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
-        self.assertIn('/lightsensors',nodes, "node does not exist"+nodes)
+        self.assertIn('/lightsensors',nodes, "node does not exist"+nodes.toString())
 
     def test_get_value(self):
         rospy.set_param('lightsensors_freq',10)
