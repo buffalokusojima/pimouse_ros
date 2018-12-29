399a3af0d8650bbb7f7f9820b51de17d6ca44342
Ubuntu <australian.in.tus@icloud.com>
2018-12-29 12:19:54 +0000

    test

--- test/travis_test_motors2.py
+++ test/travis_test_motors2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import unittest, rostest
-import risnode, rospy
+import rosnode, rospy
 import time
 from pimouse_ros.msg import MotorFreqs
 from geometory_msgs.msg import Twist
@@ -14,7 +14,7 @@ class MotorTest(unittest.TestCase):
         on = rospy.ServiceProxy('/motor_on', Trigger)
 
     def file_check(self, dev, value, message):
-        with open("/dev/" + "dev","r") as f:
+        with open("/dev/" + dev,"r") as f:
             self.assertEqual(f.readline().str(value)+"\n",message)
 
     def test_node_exist(self):
